83a2adf73239790d60f9387246de301c485402b2
Kal <info@kalacademy.org>
2020-02-23 16:21:03 -0800

    React components to api

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
